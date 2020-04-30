2ae4064a09dfa7696a60e435ccc2157e31ae24d9
Hans <48939406+WoshiiAlongKia@users.noreply.github.com>
2020-04-30 21:51:49 +0800

    added main menu

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@ using System.Text;
 using System.Collections;
 using System.Threading;
 using System.IO;
+
+//added new
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+
 namespace Snake
 {
 	//Structure Entity called Position
@@ -25,7 +31,41 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
+			
+			
+			
+			//Main menu for game
+					Console.ForegroundColor = ConsoleColor.Red;
+					string gamename = "********** SNAKE GAME **********";
+					Console.SetCursorPosition((Console.WindowWidth - gamename.Length) / 2, (Console.WindowHeight / 2) - 4);
+					Console.Write(gamename);
+					string option1 = "1.Press enter to start the game!";
+					Console.SetCursorPosition((Console.WindowWidth - option1.Length) / 2, (Console.WindowHeight / 2) - 3);
+					Console.Write(option1);
+					string option2 = "2.Press ESC to quit";
+					Console.SetCursorPosition((Console.WindowWidth - option1.Length) / 2, (Console.WindowHeight / 2) - 2);
+					Console.Write(option2);
 
+					while(true)
+					{ 
+						ConsoleKeyInfo userInput = Console.ReadKey();
+
+						if (userInput.Key == ConsoleKey.Enter) //if direction isnt equal to right it will move left
+						{
+							Console.Clear();
+						}
+	
+						if (userInput.Key == ConsoleKey.Escape) //if direction isnt equal to right it will move left
+						{
+							Console.Clear();
+							Environment.Exit(0);
+					
+					
+						}
+					break;
+					}
+		
+			
 			//adds background music to game
 			var myPlayer = new System.Media.SoundPlayer();
 			myPlayer.SoundLocation = @"bgmusic.wav";
